7f209f9ce3dda7a6af454c15d0d27d7e4ad0aceb
MunawarSaqibi <mohdmunawar.mar@gmail.com>
2026-08-14 17:10:37 +0530

    Layout in nextjs

---
+++ src/app/(offer)/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <div className="bg-[red]">
+      10%off on this Independence Day
+      {children}
+    </div>
+  );
+}
---
+++ src/app/auth/layout.jsx
@@ -0,0 +1,10 @@
+export default function AuthLayout({ children }) {
+  return (
+    <div>
+      <h1>
+        Authentication page:
+        {children}
+      </h1>
+    </div>
+  );
+}
